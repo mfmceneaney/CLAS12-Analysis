19681022760e7310db365e02dc9e9e5276d30114
Matthew McEneaney <matthew.mceneaney@duke.edu>
2022-04-08 14:12:16 -0400

    Removed additional DEBUGGING statements from `MCDecays.groovy`.

--- lib/src/main/groovy/org/jlab/analysis/MCDecays.groovy
+++ lib/src/main/groovy/org/jlab/analysis/MCDecays.groovy
@@ -298,8 +298,7 @@ public class MCDecays {
         for (ArrayList<DecayProduct> combo : this.getComboPidList()) {
             for (ArrayList<DecayProduct> check : this.getParComboPidList()) {
                 ArrayList<DecayProduct> addList = new ArrayList<DecayProduct>(combo);
-                if (combo.get(0).parent()==check.get(0).index()) { checkedComboPidList.add(addList); }
-                else { System.out.println("DEBUGGING combo.get(0).parent(), check.get(0).index()"+combo.get(0).parent()+" , "+check.get(0).index()); }//DEBUGGING
+                if (combo.get(0).parent()==check.get(0).daughter()) { checkedComboPidList.add(addList); }
             }
          }
 
