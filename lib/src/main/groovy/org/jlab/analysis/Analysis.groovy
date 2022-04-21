460ab3f26577701b956944f499fe54a719665aa6
Matthew McEneaney <matthew.mceneaney@duke.edu>
2022-04-21 16:57:24 -0400

    Added reassignment of sorted groups to reduced sorted groups in `Analysis.groovy`.

--- lib/src/main/groovy/org/jlab/analysis/Analysis.groovy
+++ lib/src/main/groovy/org/jlab/analysis/Analysis.groovy
@@ -300,7 +300,8 @@ public class Analysis {
                 }
             }
             for (Integer m : indicesToRemove.sort().reverse()) { reducedSortedParents.remove(m); } //NOTE: Remove after in descending order so you don't mess up later indices to remove.
-            
+            sortedParents = new ArrayList<Integer>(reducedSortedParents); //NOTE: Reassign!
+
             // Add parents to kinematics object
             this._kinematics.setParents(sortedParents);
         }   
