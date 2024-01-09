58859c5feafe8768a5fd7af5819e4be319c9f2f5
Matthew McEneaney <matthew.mceneaney@duke.edu>
2024-01-09 17:55:46 -0500

    Updated the setup scripts to work with new submodules.

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,7 +4,6 @@ export C12ANALYSIS=$PWD
 echo "C12ANALYSIS=$PWD" | grep C12ANALYSIS --color=auto
 
 # Setup j2root
-git clone https://github.com/drewkenjo/j2root.git
 cd j2root
 javac -h build/native src/main/java/org/jlab/jroot/JRootJNI.java
 sed -i.bak 's;env.JavaH;#env.JavaH;g' sconscript
@@ -13,7 +12,6 @@ mvn package
 cd $C12ANALYSIS
 
 # Setup clasqaDB
-git clone https://github.com/JeffersonLab/clasqaDB.git
 cd clasqaDB/src
 # jar -c -f clasqa.jar clasqa/*.groovy
 jar cf clasqa.jar clasqa/*.groovy # Depending on your jar version you might need this line instead
